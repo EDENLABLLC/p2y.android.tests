b73e60471d73c980cce68b8214029663fd937373
napster <evgeniy.furmann@gmail.com>
2016-05-12 14:48:10 +0300

    fix locators

--- pages/cards_page.js
+++ pages/cards_page.js
@@ -5,9 +5,9 @@ var cards_page = function () {
     this.assertAndfillCard = function (driver, val1, val2, val3, val4, MM, YY, CVV) {
         return driver.sleep(2000).elementById("ua.com.deltabank.pay2you:id/ll_pager").should.eventually.exist
             .elementById("ua.com.deltabank.pay2you:id/et_numbercard1").sendKeys(val1)
-            .elementById("ua.com.deltabank.pay2you:id/et_numbercard1").sendKeys(val2)
-            .elementById("ua.com.deltabank.pay2you:id/et_numbercard1").sendKeys(val3)
-            .elementById("ua.com.deltabank.pay2you:id/et_numbercard1").sendKeys(val4)
+            .elementById("ua.com.deltabank.pay2you:id/et_numbercard2").sendKeys(val2)
+            .elementById("ua.com.deltabank.pay2you:id/et_numbercard3").sendKeys(val3)
+            .elementById("ua.com.deltabank.pay2you:id/et_numbercard4").sendKeys(val4)
             .elementById("ua.com.deltabank.pay2you:id/et_validMonth").sendKeys(MM)
             .elementById("ua.com.deltabank.pay2you:id/et_validYear").sendKeys(YY)
             .elementById("ua.com.deltabank.pay2you:id/et_cvc2").sendKeys(CVV)
