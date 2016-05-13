2880b57a88209e1acb44ce4b9a3ebe3e048299af
napster <evgeniy.furmann@gmail.com>
2016-05-13 16:14:05 +0300

    card2phone test

--- pages/cards_page.js
+++ pages/cards_page.js
@@ -5,7 +5,7 @@ require('../pages/pay_page.js');
 var cards_page = function () {
 
     this.fillCard = function (driver, number, month, year, cvv) {
-        return driver.waitForElementById("ua.com.deltabank.pay2you:id/ll_pager", 2000)
+        return driver.waitForElementById("ua.com.deltabank.pay2you:id/ll_pager")
             .elementById("ua.com.deltabank.pay2you:id/et_numbercard1").sendKeys(number)
             .elementById("ua.com.deltabank.pay2you:id/et_validMonth").sendKeys(month)
             .elementById("ua.com.deltabank.pay2you:id/et_validYear").sendKeys(year)
@@ -15,6 +15,17 @@ var cards_page = function () {
 
     this.fillUserCardAndPay = function (driver, number, amount) {
         return driver.elementById("ua.com.deltabank.pay2you:id/et_cardnumber_rec_1").should.eventually.exist.sendKeys(number)
+            .elementById("ua.com.deltabank.pay2you:id/btn_enter_money").click()
+            .elementById("ua.com.deltabank.pay2you:id/transfer_layout_et_money").sendKeys(amount)
+            .elementById("ua.com.deltabank.pay2you:id/btn_PrepareTransfer").click()
+            .then(function () {
+                return require('./pay_page.js');
+            });
+    };
+
+    this.fillUserPhoneAndPay = function (driver, phone, amount) {
+        return driver.elementById("ua.com.deltabank.pay2you:id/transfer_layout_phone_transfer_select_1").click()
+            .elementById("ua.com.deltabank.pay2you:id/et_phone_1").sendKeys(phone)
             .elementById("ua.com.deltabank.pay2you:id/btn_enter_money").click()
             .elementById("ua.com.deltabank.pay2you:id/transfer_layout_et_money").sendKeys(amount)
             .elementById("ua.com.deltabank.pay2you:id/btn_PrepareTransfer").click()
--- tests/Cards2PhoneTest.js
+++ tests/Cards2PhoneTest.js
@@ -6,7 +6,7 @@ var wd = require("wd"),
 require('../helpers/setup.js');
 var fixtures = require('../fixtures');
 
-describe("Pay2You Cards2Card Tests", function () {
+describe("Pay2You Cards2Phone Tests", function () {
     this.timeout(300000);
     var driver;
     var allPassed = true;
@@ -26,7 +26,7 @@ describe("Pay2You Cards2Card Tests", function () {
 
     var pin_page = require('../pages/pin_page.js');
 
-    it("Card2Card positive", function () {
+    it("Card2Phone positive", function () {
         var card = fixtures.cards[2];
 
         return pin_page.fillPin(driver, '1111', '1111')
@@ -44,36 +44,11 @@ describe("Pay2You Cards2Card Tests", function () {
                     .then(function (cards_page) {
                         return cards_page.fillCard(driver, card.number, card.expired.month, card.expired.year, card.cvv)
                             .then(function () {
-                                return cards_page.fillUserCardAndPay(driver, card.number, '1')
+                                return cards_page.fillUserPhoneAndPay(driver, '931254212', '1')
                             })
                     })
                     .then(function (pay_page) {
                         return pay_page.payButton(driver)
-                    })
-            });
-    });
-
-    it("Card2Card bad card number", function () {
-        var card = fixtures.cards[3];
-
-        return pin_page.fillPin(driver, '1111', '1111')
-            .then(function () {
-                return pin_page.confirmButtonClick(driver)
-            })
-            .then(function (phone_page) {
-                return phone_page.fillPhoneNumber(driver, '931254212')
-            })
-            .then(function (dashboard_page) {
-                return dashboard_page.assertDashboard(driver)
-                    .then(function () {
-                        return dashboard_page.transferCard(driver)
-                    })
-                    .then(function (cards_page) {
-                        return cards_page.fillCard(driver, card.number, card.expired.month, card.expired.year, card.cvv)
-                            .then(function () {
-                                return cards_page.assertAlertMessage(driver)
-                                    .should.become('For money transfer enter your card number, please (16 digits)')
-                            })
                     })
             });
     });
